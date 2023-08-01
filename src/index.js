c41520522e1673b6f14146dbf7706d11dfb5b370
galactic-shark <evamichael100@gmail.com>
2023-08-01 18:32:02 +0800

    new json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 
